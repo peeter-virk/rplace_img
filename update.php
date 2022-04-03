0c94969a5e575645cf92ce142a2978ccf6b7864c
Endrik Einberg <endrik.einberg@gmail.com>
2022-04-04 00:36:19 +0300

    Update tõll

--- update.php
+++ update.php
@@ -1,1 +1,1 @@
-{"version":"3.33","filename":"lipp31.png","bot_version":"1.7","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v1.7.zip"}
+{"version":"3.34","filename":"lipp32.png","bot_version":"1.7","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v1.7.zip"}
