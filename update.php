e8ca1d7493b5ccc3bef35548c5011efd4f9c671c
Endrik Einberg <endrik.einberg@gmail.com>
2022-04-03 23:35:29 +0300

    Bot version update

--- update.php
+++ update.php
@@ -1,1 +1,1 @@
-{"version":"3.30","filename":"lipp29.png","bot_version":"1.5","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v1.5.zip"}
+{"version":"3.30","filename":"lipp29.png","bot_version":"1.6","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v1.6.zip"}
