930f2bb95ff710b824f20cd0f937ead114f90526
Endrik Einberg <endrik.einberg@gmail.com>
2022-04-03 23:54:10 +0300

    Kassi kõrv kaitse alla

--- update.php
+++ update.php
@@ -1,1 +1,1 @@
-{"version":"3.31","filename":"lipp30.png","bot_version":"1.6","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v1.6.zip"}
+{"version":"3.32","filename":"lipp31.png","bot_version":"1.6","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v1.6.zip"}
