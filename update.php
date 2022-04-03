628fcebde78d9d5da6cd3dbd76fe95136201c20a
peeter-virk <32205703+PeeterVirk@users.noreply.github.com>
2022-04-03 17:09:53 +0300

    g

--- update.php
+++ update.php
@@ -1,1 +1,1 @@
-{"version":"3.23","filename":"lipp23.png","bot_version":"1.4","download_link":"https://github.com/enduity/rPlaceRevamp"}
+{"version":"3.24","filename":"lipp24.png","bot_version":"1.4","download_link":"https://github.com/enduity/rPlaceRevamp"}
