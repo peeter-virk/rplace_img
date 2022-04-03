a9ecdced41f4446df2c0c8f3a5d32ff5a181426c
Endrik Einberg <endrik.einberg@gmail.com>
2022-04-03 21:55:11 +0300

    Fixed borders for all countries

--- update.php
+++ update.php
@@ -1,1 +1,1 @@
-{"version":"3.27","filename":"lipp27.png","bot_version":"1.4","download_link":"https://github.com/enduity/rPlaceRevamp"}
+{"version":"3.28","filename":"lipp28.png","bot_version":"1.4","download_link":"https://github.com/enduity/rPlaceRevamp"}
