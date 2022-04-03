5eb8f1113740a7938e6f7a90e59e52f17f1024d1
Endrik Einberg <endrik.einberg@gmail.com>
2022-04-04 01:22:55 +0300

    add others too for the night

--- update.php
+++ update.php
@@ -1,1 +1,1 @@
-{"version":"3.35","filename":"lipp33.png","bot_version":"1.7","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v1.7.zip"}
+{"version":"3.36","filename":"lipp34.png","bot_version":"1.7","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v1.7.zip"}
