57212dc04c83ba219a1e2b8477df9838a081a289
Endrik Einberg <endrik.einberg@gmail.com>
2022-04-03 23:57:50 +0300

    Paremal oli valge riba

--- update.php
+++ update.php
@@ -1,1 +1,1 @@
-{"version":"3.32","filename":"lipp31.png","bot_version":"1.6","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v1.6.zip"}
+{"version":"3.33","filename":"lipp31.png","bot_version":"1.6","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v1.6.zip"}
