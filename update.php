980d8c4be604ec0fcef77deaa9614ae9fd3cbec1
Endrik Einberg <endrik.einberg@gmail.com>
2022-04-03 21:25:47 +0300

    change borders etc

--- update.php
+++ update.php
@@ -1,1 +1,1 @@
-{"version":"3.26","filename":"lipp26.png","bot_version":"1.4","download_link":"https://github.com/enduity/rPlaceRevamp"}
+{"version":"3.27","filename":"lipp27.png","bot_version":"1.4","download_link":"https://github.com/enduity/rPlaceRevamp"}
