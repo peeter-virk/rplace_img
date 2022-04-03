e69c1a678c00fdc1eeb4699a99e93bcdaf04c7f2
Endrik Einberg <endrik.einberg@gmail.com>
2022-04-04 02:10:53 +0300

    öö 2

--- update.php
+++ update.php
@@ -1,1 +1,1 @@
-{"version":"3.37","filename":"lipp-ooseks-01.png","bot_version":"1.7","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v1.7.zip"}
+{"version":"3.38","filename":"lipp-ooseks-02.png","bot_version":"1.7","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v1.7.zip"}
