e8e727d268f1f0fbdc80037276c70488c04cb106
Endrik Einberg <endrik.einberg@gmail.com>
2022-04-04 01:01:27 +0300

    update tõll frame

--- update.php
+++ update.php
@@ -1,1 +1,1 @@
-{"version":"3.34","filename":"lipp32.png","bot_version":"1.7","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v1.7.zip"}
+{"version":"3.35","filename":"lipp33.png","bot_version":"1.7","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v1.7.zip"}
