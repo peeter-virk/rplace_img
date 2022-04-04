3ff94a93c7ef99c20b82cd07ea2dce98cb62834a
Endrik Einberg <endrik.einberg@gmail.com>
2022-04-04 03:02:01 +0300

    värvimuutus

--- update.php
+++ update.php
@@ -1,1 +1,1 @@
-{"version":"4.1","filename":"lipp-ooseks-05.png","bot_version":"1.7","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v1.7.zip"}
+{"version":"4.2","filename":"lipp-ooseks-06.png","bot_version":"1.7","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v1.7.zip"}
