fda061ab475f47f7f4251803251f90cd56077902
Endrik Einberg <endrik.einberg@gmail.com>
2022-04-05 01:29:29 +0300

    magama

--- update.php
+++ update.php
@@ -1,1 +1,1 @@
-{"version":"8.1","filename":"bot-final-loodetavasti-v2-oeh.png","bot_version":"2.0","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v2.0.zip","scan_type":"backward"}
+{"version":"8.2","filename":"magama.png","bot_version":"2.0","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v2.0.zip","scan_type":"backward"}
