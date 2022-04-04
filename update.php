0da3efbadb0d1d28d3a424d53a18cdeb6460017b
Endrik Einberg <endrik.einberg@gmail.com>
2022-04-05 00:50:38 +0300

    forward for lithuania

--- update.php
+++ update.php
@@ -1,1 +1,1 @@
-{"version":"7.9","filename":"bot-final-loodetavasti-v2-oeh.png","bot_version":"2.0","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v2.0.zip","scan_type":"random"}
+{"version":"8.0","filename":"bot-final-loodetavasti-v2-oeh.png","bot_version":"2.0","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v2.0.zip","scan_type":"forward"}
