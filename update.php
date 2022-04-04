c4ea52065e2aecadfc7105da16d1919c63b8a0c1
Endrik Einberg <endrik.einberg@gmail.com>
2022-04-04 13:53:50 +0300

    Change scan type to random

--- update.php
+++ update.php
@@ -1,1 +1,1 @@
-{"version":"5.2","filename":"maybe-final-v4.png","bot_version":"2.0","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v2.0.zip","scan_type":"forward"}
+{"version":"5.3","filename":"maybe-final-v4.png","bot_version":"2.0","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v2.0.zip","scan_type":"random"}
