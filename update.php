8bf341f47fd4da62b0036adbb2f2e826a5775a9c
Endrik Einberg <endrik.einberg@gmail.com>
2022-04-04 12:24:16 +0300

    Final? Who knows

--- update.php
+++ update.php
@@ -1,1 +1,1 @@
-{"version":"4.8","filename":"t6ll-ja-p22suke-v3.png","bot_version":"2.0","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v2.0.zip","scan_type":"random"}
+{"version":"4.9","filename":"maybe-final-v1.png","bot_version":"2.0","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v2.0.zip","scan_type":"random"}
