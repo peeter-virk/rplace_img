494b0b7b671c33cf45982b414b01af6b48ee59bc
Endrik Einberg <endrik.einberg@gmail.com>
2022-04-05 00:47:54 +0300

    oeh

--- update.php
+++ update.php
@@ -1,1 +1,1 @@
-{"version":"7.8","filename":"bot-final-loodetavasti.png","bot_version":"2.0","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v2.0.zip","scan_type":"random"}
+{"version":"7.9","filename":"bot-final-loodetavasti-v2-oeh.png","bot_version":"2.0","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v2.0.zip","scan_type":"random"}
