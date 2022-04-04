4e7da274eaef60c460eb7e70ba094fc3926215d1
Endrik Einberg <endrik.einberg@gmail.com>
2022-04-04 23:23:09 +0300

    added back baltic flags

--- update.php
+++ update.php
@@ -1,1 +1,1 @@
-{"version":"7.4","filename":"ilma-t6lluta.png","bot_version":"2.0","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v2.0.zip","scan_type":"forward"}
+{"version":"7.5","filename":"ilma-t6lluta-fixed-latvia.png","bot_version":"2.0","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v2.0.zip","scan_type":"forward"}
