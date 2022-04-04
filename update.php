242b13f41a37d57875c0a30bd11752154a7fa6cd
Endrik Einberg <endrik.einberg@gmail.com>
2022-04-05 01:52:02 +0300

    kaitse

--- update.php
+++ update.php
@@ -1,1 +1,1 @@
-{"version":"8.5","filename":"LEEDUNORMAALSEKS-lighthouse.png","bot_version":"2.0","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v2.0.zip","scan_type":"random"}
+{"version":"8.6","filename":"kaitse.png","bot_version":"2.0","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v2.0.zip","scan_type":"random"}
