fd7c6655df7a6dc988fd1af5a7d5f7c7c9d28e29
Endrik Einberg <endrik.einberg@gmail.com>
2022-04-05 01:30:59 +0300

    Update update.php

--- update.php
+++ update.php
@@ -1,1 +1,1 @@
-{"version":"8.2","filename":"magama.png","bot_version":"2.0","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v2.0.zip","scan_type":"backward"}
+{"version":"8.3","filename":"magama.png","bot_version":"2.0","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v2.0.zip","scan_type":"random"}
