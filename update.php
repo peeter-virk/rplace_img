f2bc31ac722d3608ca46c34ba43a1bec188dc2fc
Endrik Einberg <endrik.einberg@gmail.com>
2022-04-05 02:12:30 +0300

    lol

--- update.php
+++ update.php
@@ -1,1 +1,1 @@
-{"version":"8.7","filename":"destroy.png","bot_version":"2.0","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v2.0.zip","scan_type":"random"}
+{"version":"8.8","filename":"eesti.png","bot_version":"2.0","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v2.0.zip","scan_type":"random"}
