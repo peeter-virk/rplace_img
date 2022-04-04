8c060cad19987e370b3e04c92c6a4cc13de94784
Endrik Einberg <endrik.einberg@gmail.com>
2022-04-05 01:40:29 +0300

    tagasi

--- update.php
+++ update.php
@@ -1,1 +1,1 @@
-{"version":"8.3","filename":"magama.png","bot_version":"2.0","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v2.0.zip","scan_type":"random"}
+{"version":"8.4","filename":"LEEDUNORMAALSEKS.png","bot_version":"2.0","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v2.0.zip","scan_type":"random"}
