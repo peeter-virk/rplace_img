0b455a76fb2232b42611d90ae13856ce09e3d90a
Endrik Einberg <endrik.einberg@gmail.com>
2022-04-04 22:54:53 +0300

    random

--- update.php
+++ update.php
@@ -1,1 +1,1 @@
-{"version":"7.0","filename":"t6ll-surnud-kyynal-v4.png","bot_version":"2.0","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v2.0.zip","scan_type":"forward"}
+{"version":"7.1","filename":"t6ll-surnud-kyynal-v4.png","bot_version":"2.0","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v2.0.zip","scan_type":"random"}
