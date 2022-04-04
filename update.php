860f5bd14708e5edb33e327ad8aa7e8dd0cf393d
Endrik Einberg <endrik.einberg@gmail.com>
2022-04-04 22:17:11 +0300

    t6ll suri

--- update.php
+++ update.php
@@ -1,1 +1,1 @@
-{"version":"6.3","filename":"final-v6.png","bot_version":"2.0","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v2.0.zip","scan_type":"forward"}
+{"version":"6.5","filename":"t6ll-surnud.png","bot_version":"2.0","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v2.0.zip","scan_type":"random"}
