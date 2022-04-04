102ff40b439701f58dc6e0643fcac03f7dd321b5
Endrik Einberg <endrik.einberg@gmail.com>
2022-04-04 22:42:04 +0300

    uuendan

--- update.php
+++ update.php
@@ -1,1 +1,1 @@
-{"version":"6.6","filename":"t6ll-surnud-kyynal.png","bot_version":"2.0","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v2.0.zip","scan_type":"forward"}
+{"version":"6.7","filename":"t6ll-surnud-kyynal-v2.png","bot_version":"2.0","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v2.0.zip","scan_type":"forward"}
